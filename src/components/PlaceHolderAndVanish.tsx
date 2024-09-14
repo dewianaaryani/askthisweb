d358a89c2992d7e308783ab1f08956163fe6d7ad
Dewiana Aryani Rahmat <dewianarahmat@gmail.com>
2024-09-14 21:10:00 +0700

    feat: create landing page

---
+++ src/components/PlaceHolderAndVanish.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { PlaceholdersAndVanishInput } from "./ui/placeholders-and-vanish-input";
+
+export function PlaceHolderAndVanish() {
+  const placeholders = [
+    "https://en.wikipedia.org/wiki/Mark_Zuckerberg",
+    "Who is Tyler Durden?",
+    "Where is Andrew Laeddis Hiding?",
+    "Write a Javascript method to reverse a string",
+    "How to assemble your own PC?",
+  ];
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
+  return (
+    <div className="mt-10">
+      
+      <PlaceholdersAndVanishInput
+        placeholders={placeholders}
+        onChange={handleChange}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,8 @@
 import type { Config } from "tailwindcss"
+const svgToDataUri = require("mini-svg-data-uri");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 const {nextui} = require("@nextui-org/react");
 const config = {
   darkMode: ["class"],
@@ -75,7 +79,22 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), nextui()],
-} satisfies Config
+  plugins: [
+    require("tailwindcss-animate"), 
+    nextui(),
+    function ({ matchUtilities, theme }: any) {
+      matchUtilities(
+        {
+          "bg-dot-thick": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
+} satisfies Config;
 
-export default config
+export default config;
