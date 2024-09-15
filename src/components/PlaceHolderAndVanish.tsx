89696d843a161b7b7f1d11f47aa102be9b851bf6
Dewiana Aryani Rahmat <dewianarahmat@gmail.com>
2024-09-15 20:15:55 +0700

    add placeholder

--- src/components/PlaceHolderAndVanish.tsx
+++ src/components/PlaceHolderAndVanish.tsx
@@ -5,10 +5,9 @@ import { PlaceholdersAndVanishInput } from "./ui/placeholders-and-vanish-input";
 export function PlaceHolderAndVanish() {
   const placeholders = [
     "https://en.wikipedia.org/wiki/Mark_Zuckerberg",
-    "Who is Tyler Durden?",
-    "Where is Andrew Laeddis Hiding?",
-    "Write a Javascript method to reverse a string",
-    "How to assemble your own PC?",
+    "https://en.wikipedia.org/wiki/Battle_of_Britain_Day",
+    "https://www.liputan6.com/news/read/5702690/gempa-magnitudo-53-guncang-sukabumi-begini-analisis-bmkg",
+    "https:/sport.detik.com/sepakbola/liga-indonesia/d-7542012/mauro-zijlstra-ngaku-sedang-proses-naturalisasi-jadi-wni",
   ];
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
